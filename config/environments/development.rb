76a26caa0297fd08e069bd48a347d9cd7c02deb5
Ryan Mcgarvey <ryan@themcgarveys.io>
2015-11-11 13:46:15 +1100

    change to mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                  587,
+  domain:               'gmail.com',
   user_name:             ENV['GMAIL_USERNAME'],
   password:              ENV['GMAIL_PASS'],
   authentication:       'plain',
