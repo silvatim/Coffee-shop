d71b72dd67a74af86c6632c25c0b7a90e2b5610c
Ryan Mcgarvey <ryan@themcgarveys.io>
2015-11-11 13:53:49 +1100

    change to mailer again

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,20 +5,20 @@ class UserMailer < ApplicationMailer
   def order_email(order)
     @order = order
     @url  = 'http://coffeeapp.com/login'
-    mail(to: @order.email, subject: 'Your Coffee Order')
+    mail(to: @order.email, from: ENV['GMAIL_USERNAME'], subject: 'Your Coffee Order')
   end
 
 
   def confirmation_email(order)
     @order = order
     @url  = 'http://coffeeapp.com/login'
-    mail(to: @order.email, subject: 'Order Confirmation')
+    mail(to: @order.email, from: ENV['GMAIL_USERNAME'], subject: 'Order Confirmation')
   end
 
   def rejection_email(order)
     @order = order
     @url  = 'http://coffeeapp.com/login'
-    mail(to: @order.email, subject: 'Order Rejection')
+    mail(to: @order.email, from: ENV['GMAIL_USERNAME'], subject: 'Order Rejection')
   end
 
 end
